a7d13ed7ca6023d2095ec5636a827ab6e20ea4d1
Kevin O'Leary <rpskoleary@gmail.com>
2023-04-21 17:20:58 +0100

    added data set

--- analysis.py
+++ analysis.py
@@ -1,3 +1,9 @@
 # analysis.py
 # Author: Kevin O'Leary
 # Project analysing Fischer's Iris dataset
+
+# First reading in the data set to VScode. also daved in folder
+
+csv_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+data= pd.read_csv(csv_url)
